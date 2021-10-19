3f304990d28b05abef47d624b65fd7021af2304a
Rus <de.mr44@gmail.com>
2021-10-19 13:57:42 +0500

    Initial Commit

---
+++ UserDefaultsExample3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsExample3
+//
+//  Created by ruslan on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
